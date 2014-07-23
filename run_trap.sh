0bc29433d1c4c1b33aa51663b593abea1042cdf2
Gijs Molenaar <gijs@pythonic.nl>
2014-07-23 10:07:19 +0200

    get test data and latest version

--- run_trap.sh
+++ run_trap.sh
@@ -2,6 +2,23 @@
 
 git submodule init
 git submodule update
+
+pushd tkp
+# make sure we have the latest version
+git pull
+git branch
+# get the test data
+git submodule init
+git submodule update
+popd
+
+pushd banana
+# make sure we have the last version
+git pull
+git branch
+popd
+
+
 vagrant up
 vagrant ssh -c "cd /vagrant/trap_project; ./manage.py run job"
 
